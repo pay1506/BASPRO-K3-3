3caa3353c72ad86c8d30fb7ad9f666f2104876e2
pay1506 <naufaldimas421@gmail.com>
2025-03-11 23:14:30 +0700

    Add files via upload

---
+++ umur.py
@@ -0,0 +1,8 @@
+umur = int(input("Masukkan umur: "))
+
+if umur < 5: print("balita")
+elif umur < 13: print("anak-anak")
+elif umur < 25: print("remaja")
+elif umur < 35: print("dewasa")
+elif umur < 55: print("orang tua")
+elif umur > 55: print("lansia")
